cfd35f9368f70a395da797fa017e4e0872532c85
rmmcosta <ricardocosta101085@gmail.com>
2018-09-30 22:00:59 +0100

    the end for the shell 3.4 exercise

--- Exercicio 0/circuitrouter_seqsolver_ex01 modificado/inputsDescription.sh
+++ Exercicio 0/circuitrouter_seqsolver_ex01 modificado/inputsDescription.sh
@@ -4,6 +4,10 @@ for f in $(ls inputs/*.txt)
 do
     #echo $(wc -l $f) $(wc -w $f)
     lines="lines:$(wc -l $f)"
+    #the pipe passes the left output to the right input
     ps="interconnections:$(grep -o p $f | wc -l)"
-    echo $lines $words $ps | xargs -n1 | sort -u | xargs
+    #sort -u -> sort and removes duplicates
+    #-n1 use at most 1 argument per command line
+    echo $lines $ps | xargs -n1 | sort -u | xargs
+    #echo $lines $ps | xargs -n1 | sort -u
 done
